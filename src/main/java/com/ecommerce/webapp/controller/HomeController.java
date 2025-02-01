f360442d95a6b653ce2ba78816601d42d5308c82
pradip_sharmaa <sppradipsharma@gmail.com>
2025-02-01 15:47:12 +0545

    with out sign in page

--- src/main/java/com/ecommerce/webapp/controller/HomeController.java
+++ src/main/java/com/ecommerce/webapp/controller/HomeController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ecommerce.webapp.model.Product;
+import com.ecommerce.webapp.repository.ProductRepository;
 import com.ecommerce.webapp.service.ProductService;
 
 @Controller
@@ -13,6 +15,9 @@ public class HomeController {
 
 	@Autowired
     private ProductService productService;
+	
+	@Autowired
+	private ProductRepository pRepo;
 	
 
     @GetMapping("/")
@@ -31,5 +36,11 @@ public class HomeController {
     public String error() {
     	
     	return "navbar";
+    }
+    
+    @GetMapping("/search")
+    public String getSearch(@RequestParam("search") String search, Model model) {
+    	model.addAttribute("products", pRepo.findByNameContainingOrDescriptionContaining(search, search));
+    	return "home";
     }
 }
--- src/main/java/com/ecommerce/webapp/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/webapp/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.ecommerce.webapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ecommerce.webapp.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+	List<Product> findByNameContainingOrDescriptionContaining(String name, String description);
 }
